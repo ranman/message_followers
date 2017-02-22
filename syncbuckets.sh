dccf5d77b86d2fd5a00411cbe3792846c83a6c09
J. Randall Hunt <randallhunt@gmail.com>
2017-02-22 13:59:17 -0500

    update sync script

--- syncbuckets.sh
+++ syncbuckets.sh
@@ -2,5 +2,7 @@ REGIONLIST=(`echo $(aws ec2 describe-regions --output text --query 'Regions[*].R
 
 for REGION in "${REGIONLIST[@]}"; do
     aws s3 cp "message_followers.zip" "s3://randhunt-code-${REGION}" --acl public-read --region "${REGION}"
+    #aws s3 rb --force "s3://randhunt-code-${REGION}" --region "${REGION}"
+    #aws s3 mb "s3://randhunt-code-${REGION}" --region "${REGION}"
 done
-aws s3 cp "template.yaml" "s3://randhunt-code/" --acl "public-read"
+#aws s3 cp "template.yaml" "s3://randhunt-code/" --acl "public-read"
